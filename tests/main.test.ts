0403163d2ca234632b3b7c3068d00685dd5573fb
Andrei <ierdss@proton.me>
2025-09-14 19:20:06 +0800

    test(main): add tests for multiple views

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -10,18 +10,18 @@ describe("updateColumnSizeInBaseFile", () => {
 		{
 			title: "without columnSize",
 		},
-		// {
-		// 	title: "with rowHeight",
-		// },
-		// {
-		// 	title: "without rowHeight",
-		// },
-		// {
-		// 	title: "with columnSize and rowHeight",
-		// },
-		// {
-		// 	title: "without columnSize and rowHeight",
-		// },
+		{
+			title: "with rowHeight",
+		},
+		{
+			title: "without rowHeight",
+		},
+		{
+			title: "with columnSize and rowHeight",
+		},
+		{
+			title: "without columnSize and rowHeight",
+		},
 	];
 
 	describe("Insert sizes in single view", () => {
@@ -46,45 +46,82 @@ describe("updateColumnSizeInBaseFile", () => {
 		}
 	});
 
-	describe("Multiple Views ", () => {
-		const testCases = [
-			{
-				title: "Insert sizes in empty file",
-			},
-			{
-				title: "Insert sizes in fresh single view",
-			},
-		];
+	describe("Insert sizes in multiple views", () => {
+		describe("at first view", () => {
+			for (let c = 0; c < testCases.length; c++) {
+				test(`case ${c}: ${testCases[c].title}`, () => {
+					const inputPath = path.join(
+						__dirname,
+						"..",
+						"bases",
+						`case_${c + 1}/input.base`
+					);
+					const input = fs.readFileSync(inputPath);
+					const outputPath = path.join(
+						__dirname,
+						"..",
+						"bases",
+						`case_${c + 1}/output.base`
+					);
+					const output = fs.readFileSync(outputPath);
+					expect(input).toEqual(output);
+				});
+			}
+		});
 
-		for (let c = 0; c < testCases.length; c++) {
-			test(`case ${c}: ${testCases[c].title}`, () => {
-				const inputPath = path.join(
-					__dirname,
-					"..",
-					"bases",
-					`case_${c + 1}/input.base`
-				);
-				const input = fs.readFileSync(inputPath);
-				const outputPath = path.join(
-					__dirname,
-					"..",
-					"bases",
-					`case_${c + 1}/output.base`
-				);
-				const output = fs.readFileSync(outputPath);
-				expect(input).toEqual(output);
-			});
-		}
+		describe("at middle view", () => {
+			for (let c = 0; c < testCases.length; c++) {
+				test(`case ${c}: ${testCases[c].title}`, () => {
+					const inputPath = path.join(
+						__dirname,
+						"..",
+						"bases",
+						`case_${c + 1}/input.base`
+					);
+					const input = fs.readFileSync(inputPath);
+					const outputPath = path.join(
+						__dirname,
+						"..",
+						"bases",
+						`case_${c + 1}/output.base`
+					);
+					const output = fs.readFileSync(outputPath);
+					expect(input).toEqual(output);
+				});
+			}
+		});
+
+		describe("at last view", () => {
+			for (let c = 0; c < testCases.length; c++) {
+				test(`case ${c}: ${testCases[c].title}`, () => {
+					const inputPath = path.join(
+						__dirname,
+						"..",
+						"bases",
+						`case_${c + 1}/input.base`
+					);
+					const input = fs.readFileSync(inputPath);
+					const outputPath = path.join(
+						__dirname,
+						"..",
+						"bases",
+						`case_${c + 1}/output.base`
+					);
+					const output = fs.readFileSync(outputPath);
+					expect(input).toEqual(output);
+				});
+			}
+		});
 	});
 });
 
-describe("updateColumnsBySingleValue", () => {
-	test("case: 1: Update all column sizes using a single value", () => {
-		const input = updateColumnsBySingleValue(
-			{ column_1: 0, column_2: 50, column_3: 100 },
-			200
-		);
-		const output = { column_1: 200, column_2: 200, column_3: 200 };
-		expect(input).toEqual(output);
-	});
-});
+// describe("updateColumnsBySingleValue", () => {
+// 	test("case: 1: Update all column sizes using a single value", () => {
+// 		const input = updateColumnsBySingleValue(
+// 			{ column_1: 0, column_2: 50, column_3: 100 },
+// 			200
+// 		);
+// 		const output = { column_1: 200, column_2: 200, column_3: 200 };
+// 		expect(input).toEqual(output);
+// 	});
+// });
