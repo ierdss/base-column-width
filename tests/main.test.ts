4682401c3499ebefd8902df21e946f382e1141dc
Andrei <ierdss@proton.me>
2025-09-14 17:47:45 +0800

    test(main): refac test to become dynamic

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,21 +2,32 @@ import * as fs from "fs";
 import * as path from "path";
 
 describe("updateColumnSizeInBaseFile", () => {
-	test("case: Single view with columnSize", () => {
-		const inputPath = path.join(
-			__dirname,
-			"..",
-			"bases",
-			"test_1_input.base"
-		);
-		const input = fs.readFileSync(inputPath);
-		const outputPath = path.join(
-			__dirname,
-			"..",
-			"bases",
-			"test_1_output.base"
-		);
-		const output = fs.readFileSync(outputPath);
-		expect(input).toEqual(output);
-	});
+	const testCases = [
+		{
+			title: "Insert sizes in empty file",
+		},
+		{
+			title: "Insert sizes in fresh single view",
+		},
+	];
+
+	for (let c = 0; c < testCases.length; c++) {
+		test(`case ${c}: ${testCases[c].title}`, () => {
+			const inputPath = path.join(
+				__dirname,
+				"..",
+				"bases",
+				`case_${c + 1}/input.base`
+			);
+			const input = fs.readFileSync(inputPath);
+			const outputPath = path.join(
+				__dirname,
+				"..",
+				"bases",
+				`case_${c + 1}/output.base`
+			);
+			const output = fs.readFileSync(outputPath);
+			expect(input).toEqual(output);
+		});
+	}
 });
